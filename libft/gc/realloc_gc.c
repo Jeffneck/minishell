095374365fa68ec1387fcfab53fbb49ca5f89d70
Hugo Anglade <hanglade@z1r6p1.42lyon.fr>
2024-01-29 15:51:00 +0100

    add max size t value() ine realloc_gc

--- libft/gc/realloc_gc.c
+++ libft/gc/realloc_gc.c
@@ -6,11 +6,18 @@
 /*   By: hanglade <hanglade@student.42lyon.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/29 10:55:12 by hanglade          #+#    #+#             */
-/*   Updated: 2024/01/29 14:44:26 by hanglade         ###   ########.fr       */
+/*   Updated: 2024/01/29 15:48:15 by hanglade         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../libft.h"
+
+static size_t	max_value_size_t (size_t v1, size_t v2)
+{
+	if(v1 > v2)
+		return (v1);
+	return (v2);
+}
 
 void	*realloc_gc(void *ptr, size_t prev_size, size_t new_size, int id_gc)
 {
@@ -19,7 +26,7 @@ void	*realloc_gc(void *ptr, size_t prev_size, size_t new_size, int id_gc)
 
 	if (new_size == 0)
 		return (del_one_garbage(ptr, id_gc), NULL);
-	size = (size_t) max_value(prev_size, new_size, sizeof(size_t));
+	size = (size_t) max_value_size_t(prev_size, new_size);
 	new = malloc_gc(size, id_gc);
 	if(ptr)
 		ft_memcpy(new, ptr, size);
