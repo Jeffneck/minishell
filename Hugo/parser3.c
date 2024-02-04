202f26da17400746936a20768da1ae20a8d2d081
Hugo <hugot.anglade@gmail.com>
2024-02-04 16:50:21 -0500

    add homework on expander ..

--- Hugo/parser3.c
+++ Hugo/parser3.c
@@ -1,16 +1,5 @@
 # include "lexer.h"
 
-typedef	struct s_args
-{
-	char **content;
-	t_args *
-	int	io[2]; //in out (peut etre compose de pipe)
-	int	branch;
-	token_type	type;
-	struct s_btree	*left;
-	struct s_btree	*right;
-}	t_btree;
-
 typedef	struct s_btree
 {
 	char **content;
@@ -47,11 +36,13 @@ t_btree	*create_bin_tree(t_lister *list_tkn)
 	while(list_tkn->head)
 	{
 		target = find_prior_token(list_tkn);
-		target = expand()
-		if (root == NULL)
+		if (!root)
 		 	root = place(root, btree_new(target), target->index);
 		else if (target->type == PARENTHESE)
+		{
+			// verify_syntax_inside_parenthesis
 			place(root, parser(lexer(target->content)), target->index);
+		}
 		else
 			place(root, btree_new(target), target->index);
 	}
@@ -98,39 +89,106 @@ void	verify_syntax(t_lister *list_tkn)
 	}
 }
 
-
-
-char	*apply_expand_dollar(char *str, char *p_dollar)
+char	*replace_substr(char *str, char *replacement, size_t start, size_t length)
 {
-	size_t	i;
-	char	*expansion;
+	char	*new;
+	size_t	size_new;
 
-	i = 1;
-	while(p_dollar[i] && !is_separator(p_dollar[i]))
-		/////////////////
-		expansion = //
-		replace_var(str, &str[i], expansion);
+	size_new = (ft_strlen(str) - length + ft_strlen(replacement)) + 1;
+	new = (char *) malloc_gc(size_new, ID);
+	if (new == NULL)
+		return (NULL);// appliquer gestion d' erreur
+	ft_memcpy(new, str, start); //ou start + 1 ou start - 1
+	ft_strcat(new, replacement);
+	ft_strcat(new, &str[start + length]); //valider
+	return (new);
+}
+
+char	*replace_endstr(char *str, char *replacement, size_t end_pos)
+{
+	//utiliser pour $var
+	char	*new;
+	size_t	len_str;
+	size_t	size_new;
+
+	if (!str || !replacement)
+		return (NULL);
+	len_str = ft_strlen(str);
+	size_new = (len_str - (len_str - end_pos) + ft_strlen(replacement) + 1) * sizeof(char);
+	new = (char *) malloc_gc(size_new, ID);
+	if (!new)
+		return (NULL);// appliquer gestion d' erreur
+	ft_memcpy(new, str, end_pos); //ou end_pos + 1 ou end_pos - 1
+	ft_strcat(new, replacement);
+	return (new);
+}
+
+char	*apply_expand_dollar(char *str, char *p_dollar, size_t pos_dollar) // un espace peut separer la var de la suite du txt donc utiliser replace sub
+{
+	char	*expansion;
+	char	*new;
+
+	expansion = getenv(&str[1]); //il ne faut pas liberer la memoire
+	if (!expansion)
+		new = replace_endstr(str, &str[0], "\n");
+	else
+		new = replace_endstr(str, &str[0], expansion);
+	return (new);
+}
+
+char	*expand_tilde(char *str, size_t start) //tilde n a pas besoin de start 
+{
+	char	*expansion;
+	char	*new;
+	size_t	len_sub;
+
+	len_sub = 2;
+	expansion = getenv("HOME"); //il ne faut pas liberer la memoire
+	if (!expansion)
+		new = replace_substr(str, "\n", start, len_sub);
+	else
+		new = replace_substr(str, expansion, start, len_sub);
+	return (new);
+}
+
+char	*expand_lastcmd_var(char *str, size_t start)
+{
+	char	*expansion;
+	char	*new;
+	size_t	len_sub;
+
+	len_sub = 2;
+	expansion = getenv("HOME"); //il ne faut pas liberer la memoire
+	if (!expansion)
+		new = replace_substr(str, "\n", start, len_sub);
+	else
+		new = replace_substr(str, expansion, start, len_sub);
+	return (new);
 }
 
 
-char	*expand_type_word(char *str)
+char	*handle_wordtype_expand(char *str)
 {
 	//*
 	//$ /$?
 	//~ => est expand seulement si c' est le 1er char du word
 
 	char	*new;
-	int	i;
+	size_t	i;
 
 	i = 0;
 	if (str[i] == '~')
 		new = expand_tilde(&str[i]);
 	while (str[i])
 	{
-		if (str[i] == '$')
-			new = expand_dollar(&str[i]);
-		if (str[i] == '*')
+		if (ft_strcmp(&str[i], "$?") == 0)
+			new = expand_lastcmd_var(str, &str[i]);
+		else if (str[i] == '$')
+			new = expand_dollar(str, &str[i], i);
+		else if (str[i] == '*')
 			new = expand_wildcard(&str[i]);
+		else if (str[i] == '~')
+			new = expand_tilde(&str[i]);
 	}
 	return (new);
 }
@@ -142,6 +200,7 @@ char	**expand_type_dquotes(char *str)
 	int	i;
 
 	i = 0;
+	
 	while (str[i])
 	{
 		if (str[i] == '$')
@@ -154,8 +213,7 @@ char	**expand_type_dquotes(char *str)
 	
 }
 
-
-void expander(t_lister *list_tkn)
+void	expander(t_lister *list_tkn)
 {
 	t_token	*curr;
 
@@ -173,11 +231,20 @@ void expander(t_lister *list_tkn)
 	}
 
 }
+void	verify_syntax(t_lister *list_tkn)
+{
+	//un token parenthese ne peut qu etre au debut ou a la fin de la ligne de cmd
+	//et etre precede ou succede par un operateur | || &&
+	t_token *curr;
 
+	curr = list_tkn->head;
+
+}
 t_btree	*parser(t_lister *list_tkn)
 {
-	verify_syntax()
+	verify_syntax(list_tkn);
 	expander(list_tkn);
+	linker(list_tkn); //regroupe plusieurs nodes linkes en 1 seul
 	set_token_index(list_tkn);
 	create_bin_tree(list_tkn);
 
--- libft/gc/strndup_gc.c
+++ libft/gc/strndup_gc.c
@@ -12,23 +12,26 @@
 
 #include "../libft.h"
 
-char	*strdup_gc(const char *str, int id_gc)
+char	*strndup_gc(const char *str, size_t size, int id_gc)
 {
 	char	*result;
 	size_t	length;
+	size_t	i;
 
 	if (!str)
 		return (NULL);
 	length = ft_strlen(str);
+	if(size < length)
+		length = size;
 	result = (char *)malloc_gc((length + 1) * sizeof(char), id_gc);
 	if (!result)
 		return (NULL);
-	length = 0;
-	while (str[length] != '\0')
+	i = 0;
+	while (i < length)
 	{
-		result[length] = str[length];
-		length++;
+		result[i] = str[i];
+		i++;
 	}
-	result[length] = '\0';
+	result[i] = '\0';
 	return (result);
 }
