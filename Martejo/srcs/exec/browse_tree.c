6ff37bfc44fa775f8094758bf9f3095549139371
Hugo <hugot.anglade@gmail.com>
2024-02-23 10:47:48 -0500

    add heredoc management in btree and func to know if a cmd is in a pipe

--- Martejo/srcs/exec/browse_tree.c
+++ Martejo/srcs/exec/browse_tree.c
@@ -13,7 +13,7 @@ void traverse_heredoc_node(t_mini *mini, t_btree *tree_el, t_io io_inherited)
 	while (line)
 	{
 		// Vérifie si le délimiteur a été entré
-        if (ft_strcmp(line, tree_el->content) == 0) // attention aux sauts de lignes contenus dans get next line qui peuvent faire foirer la cmp
+        if (ft_strcmp(line, tree_el->cmds[0] == 0)) // attention aux sauts de lignes contenus dans get next line qui peuvent faire foirer la cmp
 		{
 			free(line);
             break;
@@ -36,7 +36,7 @@ void traverse_redir_input_node(t_mini *mini, t_btree *tree_el, t_io io_inherited
 {
 	t_io	io_transmitted;
 	ft_memcpy(&io_transmitted, &io_inherited, sizeof(t_io));
-	io_transmitted.fd_in = open(tree_el->content, O_RDONLY);
+	io_transmitted.fd_in = open(tree_el->cmds[0], O_RDONLY);
 	if (io_transmitted.fd_in == -1)
 		exit(EXIT_FAILURE);//
 	browse_tree(mini, tree_el->left, io_transmitted);
@@ -47,9 +47,9 @@ void traverse_redir_output_node(t_mini *mini, t_btree *tree_el, t_io io_inherite
 	t_io	io_transmitted;
 	ft_memcpy(&io_transmitted, &io_inherited, sizeof(t_io));
 	if(tree_el->type == OUT)
-		io_transmitted.fd_out = open(tree_el->content, O_WRONLY | O_CREAT | O_TRUNC, 0644);
+		io_transmitted.fd_out = open(tree_el->cmds[0], O_WRONLY | O_CREAT | O_TRUNC, 0644);
 	else
-		io_transmitted.fd_out = open(tree_el->content, O_APPEND | O_CREAT, 0644);
+		io_transmitted.fd_out = open(tree_el->cmds[0], O_APPEND | O_CREAT, 0644);
 	if (io_transmitted.fd_out == -1)
 		exit(EXIT_FAILURE);//
 	browse_tree(mini, tree_el->left, io_transmitted);
@@ -63,11 +63,11 @@ void traverse_pipe_node(t_mini *mini, t_btree *tree_el, t_io io_inherited)
 	if(pipe(fd_pipe) == -1)
 		exit(EXIT_FAILURE);//
 	io_transmitted.fd_out = fd_pipe[FD_WRITE];
-	browse_tree(mini, tree_el->left, io_transmitted);
+	browse_tree(mini, tree_el->left, io_transmitted); //cmd 1
 	close(fd_pipe[FD_WRITE]);
 	io_transmitted.fd_in = fd_pipe[FD_READ];
 	io_transmitted.fd_out = io_inherited.fd_out;
-	browse_tree(mini, tree_el->right, io_transmitted);
+	browse_tree(mini, tree_el->right, io_transmitted); //cmd2
 	close(fd_pipe[FD_READ]);
 
 }
@@ -77,6 +77,32 @@ void traverse_logical_op_node(t_mini *mini, t_btree *tree_el, t_io io_inherited)
 	browse_tree(mini, tree_el->left, io_inherited);
 	if ((tree_el->type == AND && g_status == 0) || (tree_el->type == OR && g_status != 0))
 		browse_tree(mini, tree_el->right, io_inherited);
+}
+
+void exec_cmd_node(t_mini *mini, t_btree *tree_el, t_io io_inherited)
+{
+	if (is_builtin(tree_el->cmds[0]))
+		g_status = exec_builtin(mini, tree_el, io_inherited); //me renvoie le code d'erreur.
+	g_status = exec_bin(mini, tree_el, io_inherited);
+}
+
+
+//btree utils
+cmd_is_inside_pipe(t_btree *root, int branch_id)
+{
+	t_btree	*curr;
+
+	curr = root;
+	while (curr && curr->branch != branch_id)
+	{
+		if (curr->type == PIPE)
+			return (1);
+		if (branch_id < curr->branch)
+			curr = curr->left;
+		else
+			curr = curr->right;		
+	}
+	return (0);
 }
 
 void browse_tree(t_mini *mini, t_btree *tree_el, t_io io_inherited)
@@ -91,6 +117,7 @@ void browse_tree(t_mini *mini, t_btree *tree_el, t_io io_inherited)
 		traverse_redir_input_node(mini,tree_el, io_inherited);
 	else if(tree_el->type == OUT || tree_el->type == APPEND)
 		traverse_redir_output_node(mini,tree_el, io_inherited);
-	else if(tree_el->type == WORD || tree_el->type == ONE_QUOTE || tree_el->type == TWO_QUOTE) //transformer toutes les commandes en type word serait plus simple, ou fonction tree_el_is_cmd()
-		fork_exec(mini->env, tree_el, io_inherited);
+	else if(tree_el->type == WORD)
+		exec_cmd_node(mini,tree_el, io_inherited);
+
 }
